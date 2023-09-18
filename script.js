80ff7bd272698a982a3a4a15ae479bc3b6789747
miih244 <114492877+miih244@users.noreply.github.com>
2023-09-18 12:41:05 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-alert("seja bem vindo");
+function () {
+    alert("botao pom clicado");
+}
+document.querySelector(`.tecla_pom`).onclick = pom;
+
+function clap() {
+    alert("botao clap foi clicado")
+}
+document.querySelector(`.tecla_clap`).onclick = clap;
