1c89e4e6b64f37101f70320ca5ff79511f6c7163
miih244 <114492877+miih244@users.noreply.github.com>
2023-09-25 12:40:09 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,45 @@
+alert("olá");
+function pom(){
+document.querySelector('#som_tecla_pom').play();
+}
+document.querySelector('tecla_pom').onclick = pom;
+
+function clap(){
+document.querySelector('#som_tecla_clap').play();
+}
+document.querySelector('tecla_clap').onclick = clap;
+
+function tim(){
+document.querySelector('#som_tecla_tim').play();
+}
+document.querySelector('tecla_tim').onclick = tim;
+
+function puff(){
+document.querySelector('#som_tecla_puff').play();
+}
+document.querySelector('tecla_puff').onclick = puff;
+            
+function splash(){
+document.querySelector('#som_tecla_splash').play();
+}
+document.querySelector('tecla_splash').onclick = splash;
+
+function toim(){
+document.querySelector('#som_tecla_toim').play();
+                    }
+document.querySelector('tecla_toim').onclick = toim;
+
+function psh(){
+document.querySelector('#som_tecla_psh').play();
+                        }
+document.querySelector('tecla_psh').onclick = psh;
+    
+function tic(){
+document.querySelector('#som_tecla_tic').play();
+                        }
+document.querySelector('tecla_tic').onclick = tic;
+    
+function tom(){
+document.querySelector('#som_tecla_tom').play();
+                            }
+document.querySelector('tecla_tom').onclick = tom;
